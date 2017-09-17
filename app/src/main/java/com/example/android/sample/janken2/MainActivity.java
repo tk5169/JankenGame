7500f43bce34aa6cfcd187d6ad188fab2fd317fd
takenaka <morissa516@yahoo.co.jp>
2017-09-17 17:11:18 +0900

    アプリ起動時の最初の画面の変更

--- app/src/main/java/com/example/android/sample/janken2/MainActivity.java
+++ app/src/main/java/com/example/android/sample/janken2/MainActivity.java
@@ -1,12 +1,22 @@
+package com.example.android.sample.janken2;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.android.sample.janken2.R;
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_select);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
